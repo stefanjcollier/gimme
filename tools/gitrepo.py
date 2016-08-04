d71769ee44a4a78d1c2ad2ab76d0e49aaf6b9d38
Stefan Collier <t-stcollier@expedia.com>
2016-08-04 15:24:52 +0100

    V1 included the tools that allow ensure the relevant files exist

--- tools/gitrepo.py
+++ tools/gitrepo.py
@@ -1,9 +1,17 @@
 from os import listdir
-from os.path import join, isdir
+from os.path import join, isdir, isfile
+from core import outi
+import os
 
+# The location of the file that says where the git repo is stored
+git_home_loc_path = '%s/.stools_config/gimme/git_home_loc.txt' % os.path.expanduser('~')
 
-def locate_git():
-    return '/Users/stcollier/Git/'
+"""
+    Asks the user for the git home
+"""
+def define_git_home():
+    git_loc_file = open(git_home_loc_path, 'w')
+    git_loc_file.write(raw_input("gimme: Please enter your the location of the folder holding all your git repos\n>"))
 
 """
     A dir is a repo if it contains a .git folder within it
@@ -14,8 +22,21 @@ def is_repo(path):
 """
     Returns all the folders that are git repos
 """
-def get_repos(git):
+def get_repos():
+    git = locate_git()
     dirs = listdir(git)
     return [dir for dir in dirs if isdir(join(git,dir)) and is_repo(join(git,dir))]
 
+#Ensure file exists and git_home is a real git folder
+if not isfile(git_home_loc_path):
+    define_git_home()
+
+git_home = open(git_home_loc_path,'r').readline()
+if not git_home:
+    out('The path to your git home is empty, please enter a new one')
+    define_git_home()
+
+elif not isdir(git_home):
+    out('The chosen path for your git home is not a valid path, please enter a new one')
+    define_git_home()
 
--- tools/hist.py
+++ tools/hist.py
@@ -1,15 +1,6 @@
 from gitrepo import is_repo
 from os.path import isfile
 
-file_loc = None
-
-"""
-    Initialize the file_loc based on something 
-"""
-def init_file_loc():
-    global file_loc
-    file_loc = '/Users/stcollier/.gimme.properties'
-
 """
     Get the location of the repo file
 """
@@ -21,8 +12,6 @@ def locate_file():
 """
 def assert_file():
     init_file_loc()
-    if not isfile(locate_file()):
-        save_selected_git_repo('')
 
 """
     Get the path of the last repo that was 'gimme'd
@@ -42,4 +31,6 @@ def save_selected_git_repo(repo_path):
     repo_file.close()
     print "Path set to %s" % repo_path
 
-
+file_loc = '%s/.stools_config/gimme/gimme_hist.txt' % os.path.expanduser('~')
+if not isfile(locate_file()):
+    save_selected_git_repo('')
