e23b64d797e259159f9400db39880af5d7a35dd6
Stefan Collier <sc22g13@soton.ac.uk>
2016-08-15 21:01:52 +0100

    #7 - Removed redundant code

--- install_gimme.sh
+++ install_gimme.sh
@@ -11,20 +11,9 @@
 #           |-gimme_hist.txt
 #
 # Usage:
-#    ./install_gimme.sh <path>      #Installs the gimme command
+#    ./install_gimme.sh       #Installs the gimme command
 #
-#  where <path> is the path to the users folder with all their git repos
 #---
-if [ -z $1 ]; then
-    echo 'You forgot the first arguement!'
-    echo ''
-    echo 'Usage:'
-    echo '   ./install_gimme.sh <path>      Installs the gimme command' 
-    echo ''
-    echo 'Where <path> is the path to the users folder with all their git repos'
-
-    exit -2
-fi
 echo '============================='
 echo '      Installing gimme'
 echo '============================='
@@ -33,25 +22,13 @@ mkdir ~/.stools_config && true
 mkdir ~/.stools_config/gimme && true
 
 echo '+Creating files'
-touch ~/.stools_config/gimme/git_home_loc.txt
 touch ~/.stools_config/gimme/gimme_hist.txt
 
-echo '+Saving git home to file'
-git_home=$1
-if [ ! -d $git_home ]; then
-    echo "install_gimme: '${git_home}' is not a valid directory"
-    exit -1
-fi
-
-echo $git_home > ~/.stools_config/gimme/git_home_loc.txt
 echo 'install_gimme: Install complete'
-
-echo ''
 echo ''
 echo '============================='
 echo '  Add this to your .bashrc'
 echo '============================='
-
 echo ''
 echo "function gimme {"
 echo "   python ${PWD}/gimme.py \$*"
--- tools/gitrepo.py
+++ tools/gitrepo.py
@@ -1,29 +1,12 @@
 from os import listdir
-from os.path import join, isdir, isfile
-from core import out
 import os
 import subprocess
-
-
-# The location of the file that says where the git repo is stored
-git_home_loc_path = '%s/.stools_config/gimme/git_home_loc.txt' % os.path.expanduser('~')
-
-"""
-    Asks the user for the git home
-"""
-def define_git_home():
-    inp = raw_input("gimme: Please enter your the location of the folder holding all your git repos\n>")
-    git_loc_file = open(git_home_loc_path, 'w')
-    git_loc_file.write(inp)
 
 """
     A dir is a repo if it contains a .git folder within it
 """
 def is_repo(path):
     return '.git' in listdir(path)
-
-def locate_git():
-    return git_home
 
 """
     Returns all the folders that are git repos
@@ -36,17 +19,5 @@ def get_repos():
         '.git']).split('\n')[:-1]
     return (git_path[:-4] for git_path in output) 
 
-# Ensure file exists and git_home is a real git folder
-if not isfile(git_home_loc_path):
-    define_git_home()
 
-# Ensure the location in the file is valid
-git_home = open(git_home_loc_path,'r').readline().rstrip()
-if not git_home:
-    out('The path to your git home is empty, please enter a new one')
-    define_git_home()
-
-elif not isdir(git_home):
-    out('The chosen path (\'%s\') for your git home is not a valid path, please enter a new one' % git_home)
-    define_git_home()
 
